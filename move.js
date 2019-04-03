630bd0302ed47d77735a2925de912ed684943dbf
Kinoyui <k.y.reo.rai@gmail.com>
2019-04-03 13:14:19 +0900

    a

--- move.js
+++ move.js
@@ -14,4 +14,20 @@ $(function(){
       return false;
    });
 });
-</script>
+
+
+var _window = $(window),
+    _header = $('header'),
+    heroBottom;
+
+_window.on('scroll',function(){
+    heroBottom = $('.top').height();
+    if(_window.scrollTop() > heroBottom){
+        _header.addClass('transform');
+    }
+    else{
+        _header.removeClass('transform');
+    }
+});
+
+_window.trigger('scroll');
